0a4aefe2d4cc075a849b1808c8b37ae334aafd8e
infirit89 <georgedautov808@gmail.com>
2024-09-06 22:51:39 +0300

    worked around a bug with tauri, where the window flashes white when opened

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,24 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::{thread::sleep, time::Duration};
+
+use tauri::{Manager, Window};
+
+#[tauri::command]
+async fn show_window(window: Window) {
+    // Show main window
+    sleep(Duration::from_millis(500));
+    window
+        .get_window("main")
+        .expect("no window labeled 'main' found")
+        .show()
+        .unwrap();
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![show_window])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import '@fontsource/inter';
+import './index.css';
+import InitColorSchemeScript from '@mui/joy/InitColorSchemeScript';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <InitColorSchemeScript />
     <App />
   </StrictMode>,
 )
